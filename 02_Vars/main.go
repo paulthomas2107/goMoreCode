8690452e67cec983d31af96cb459fe457ad7b898
Paul Thomas <paulthomas2107@gmail.com>
2021-02-22 22:18:01 +0000

    Init..

---
+++ 02_Vars/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// Main Types
+	// String
+	// bool
+	// int int8 int16 int32 int64
+	// uint uint8 uint16 uint32 uint64 unintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex128
+
+}
